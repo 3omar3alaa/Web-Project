87c3c1900cdedccc709853bd16f7c5776510ad3a
Omar Alaa <omaralaa1996@gmail.com>
2018-12-23 12:14:18 +0200

    Some Modifications to Back end
    User can push a place and view his place from the database and then return to the home page

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const fs = require('fs'); 
 
 // setup express app
 const app = express();
+
 // setup view Engine
 // app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs'); 
+
 // setup public files
-// app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
 // Connect to database
 mongoose.connect('mongodb://localhost/airdb', { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
 
+
+
+
+// app.set('view engine', 'html'); 
+
 app.use(bodyParser.urlencoded({
   extended: true
-}));
+})); 
 
 // transform all comming requests into json format
 app.use(bodyParser.json());
@@ -26,12 +35,24 @@ app.use(bodyParser.json());
 
 
 // Controllers
-app.use('/api/admin', require('./Controllers/admin'));
-app.use('/api/owner', require('./Controllers/owner'));
+app.use('/admin', require('./Controllers/admin'));
+app.use('/owner', require('./Controllers/owner'));
 // app.use('/api/tenant', require('./Controllers/tenant'));
-// app.get('/*', (req, res) => {
-// 	res.render('index.html');
-// });
+app.get('/*', (req, res) => {
+    console.log(req.url);
+    if (req.url == '/'){
+        res.render('cart.ejs');
+    }
+    else{
+        data= fs.readFile('./Views/' + req.url,   function (err, data) {
+        res.setHeader('Content-Type', 'text/html');
+        res.send(data);})
+    }
+    // res.render('cart.ejs');
+    // data= fs.readFile('./Views/' + req.url,   function (err, data) {
+    // res.setHeader('Content-Type', 'text/html');
+    // res.send(data);})
+});
 
 // Listening
 app.listen(process.env.PORT || 3000, () => {
